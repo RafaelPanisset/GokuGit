7ac18a42ad3ef93c1cdc658b0000c0ca7173b436
Rafael Panisset <rafaelpani99@gmail.com>
2024-06-23 20:30:56 -0300

    Fix init file

--- commands/init.py
+++ commands/init.py
@@ -16,11 +16,7 @@ def initialize_goku_repository(path):
     os.makedirs(goku_path, exist_ok=True)
      
     for file_name, content in goku_structure.items():
-        if file_name in ["objects", "refs"]:
-            os.makedirs(os.path.join(goku_path, file_name), exist_ok=True)
-        elif file_name in ["hooks", "info"]:
-            os.makedirs(os.path.join(goku_path, file_name), exist_ok=True)
-        elif content is None:
+        if file_name in ["objects", "refs", "hooks", "info"] or content is None:
             os.makedirs(os.path.join(goku_path, file_name), exist_ok=True)
         else:
             create_file(os.path.join(goku_path, file_name), content)
@@ -28,5 +24,10 @@ def initialize_goku_repository(path):
     # Create the refs/heads directory
     refs_heads_path = os.path.join(goku_path, 'refs', 'heads')
     os.makedirs(refs_heads_path, exist_ok=True)
+
+    # Ensure HEAD file is created correctly
+    head_path = os.path.join(goku_path, 'HEAD')
+    with open(head_path, 'w') as head_file:
+        head_file.write("ref: refs/heads/master\n")
 
     print(f"Initialized empty Goku repository in {os.path.abspath(path)}")
